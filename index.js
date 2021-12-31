4c58319143af6869ff1517966d0366dd5cc617b1
Gabriel Malek <malek.gabriel33@gmail.com>
2021-12-31 13:09:04 -0800

    Added error handling

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const puppeteer =require('puppeteer');
 const { threadId } = require('worker_threads');
 const fs = require('fs').promises;
+//let url = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442'
+let url = "https://www.google.com/"
 
-
-
- const mainFunction = async () => {
+ const mainFunction = async (url) => {
     const browser = await puppeteer.launch({
         headless: true,
         //  args: ['--single-process','--no-zygote','--no-sandbox']
@@ -17,25 +17,61 @@ const fs = require('fs').promises;
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
     console.log('page opened')
-    // await page.screenshot({path:'./google.png'})
-    // page.on('requestfailed', request => {
-    //     console.log(`url: ${request.url()}, errText: ${request.failure().errorText}, method: ${request.method()}`)
-    // });
+
+    page.on('requestfailed', async (request) => {
+        // console.log(`url: ${request.url()}, errText: ${request.failure().errorText}, method: ${request.method()}`);
+        // console.log('request failed')
+        // console.log(request.url())
+        if (request.url == url){
+            await page.evaluate(() => window.stop());
+            await browser.close();
+        }
+           
+    });
     // page.on("pageerror", err => {
     //     console.log(`Page error: ${err.toString()}`);
     // });
     // Catch all console messages
+    // page.on("error", function (err) {  
+    //     theTempValue = err.toString();
+    //     console.log("Error something has gone wrong");
+    //     page.evaluate(() => window.stop());
+    //     browser.close(); 
+    // });
+    page.goto(url).catch(async(err) => {
+            console.log('Navigation failure');
+            await page.screenshot({path:'./images/goto-error.png'})
+            await page.evaluate(() => window.stop());
+            await browser.close();
+        });
 
-    page.goto('https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442');
-    // page.goto('https://www.bestbuy.com/site/hot-wheels-worldwide-basic-car-styles-may-vary/6151804.p?skuId=6151804');
-    // await page.screenshot({path:'./google.png'})
-    console.log('navigation succesful');
-    await page.waitForSelector('.fulfillment-add-to-cart-button div div button');
-    let inStockState = await page.evaluate(() => {
+        await page.screenshot({path:'./images/product-page.png'}).catch(async(err) => {
+            console.log('target closed');
+            pageFailure = "true";
+            await page.evaluate(() => window.stop());
+            await browser.close();
+        });
+        if (typeof pageFailure !== 'undefined') console.log(pageFailure);
+        if (typeof pageFailure != 'undefined' && pageFailure == 'true'){return};
+
+        await page.waitForSelector('.fulfillment-add-to-cart-button div div button').catch(async(err) => {
+            console.log('target closed');
+            pageFailure = "true";
+            await page.screenshot({path:'./images/element-error.png'})
+            await page.evaluate(() => window.stop());
+            await browser.close();
+        })
+
+    if (typeof pageFailure !== 'undefined') console.log("This site isnt supported brah");
+    if (typeof pageFailure != 'undefined' && pageFailure == 'true'){return};
+
+        console.log('navigation succesful');
+        let inStockState = await page.evaluate(() => {
         let button = document.querySelector('.fulfillment-add-to-cart-button div div button').innerText;
         button = button.toString();
         return button
-    })
+    }).catch((err) => {console.log('target closed')})
+    
     console.log(inStockState);
     if (inStockState === "Add to Cart"){
         console.log("hell yeah bitches");
@@ -49,14 +85,16 @@ const fs = require('fs').promises;
         // await browser.close();
     }else { 
         console.log("sad face");
-        mainFunction();
-        await page.evaluate(() => window.stop());
-        await browser.close();
+        mainFunction(url);
     }
+    
+        
+        
+    
     
     };
     
-mainFunction();
+mainFunction(url);
    
         // await page.goto('https://www.google.com/');
 
